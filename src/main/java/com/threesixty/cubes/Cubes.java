d7cbfd2b27d9606481bbe069c5d2c5f93ac47e8c
Bekir Doğru <bekirdogru@gmail.com>
2016-01-24 14:06:42 +0200

    First Commit

---
+++ src/main/java/com/threesixty/cubes/Cubes.java
@@ -0,0 +1,13 @@
+package com.threesixty.cubes;
+
+/**
+ * Hello world!
+ *
+ */
+public class Cubes 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
